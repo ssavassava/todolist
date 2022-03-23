2e31dafa7bc648e7c8dfecbbad307096409105fd
plato <elephamaximu@gmail.com>
2022-03-23 16:46:09 +0900

    express app listen 코드 추가

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function (req, res) {
+	res.send('hello world');
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+	console.log(`Server is running at ${PORT}`);
 });
