e425fa1487cba5588bfc5474bbf3148a593aa27f
ssavassava <sangwo98.kang@gmail.com>
2022-03-24 10:40:13 +0900

    READ 파트 작성

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
+const dbconfig   = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-	res.send('hello world');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// ROOT
+app.get('/', (req, res) => {
+  res.send('Root route');
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
-	console.log(`Server is running at ${PORT}`);
+// CREATE
+app.post('/create-user', (req, res) => {
+  const uid = req.body;
+  console.log(uid);
+  connection.query("INSERT INTO user (user_id) VALUES ('" + uid + "')", (error, rows) => {
+    if (error) throw error;
+    res.send("Insert " + uid + " success");
+  });
+});
+
+// READ
+app.get('/read-user', (req, res) => {
+  const uid = req.query.uid;
+  console.log(uid);
+  connection.query("SELECT content, completed FROM todo WHERE user_id='" + uid + "'", (error, rows) => {
+    if (error) throw error;
+    // console.log('Todo List is: ', rows);
+    res.send(rows);
+  });
+});
+
+// UPDATE
+/*
+app.post('/update-todo', (req, res) => {
+  const uid = req.body;
+  console.log(uid);
+  connection.query("INSERT INTO user (user_id) VALUES ('" + uid + "')", (error, rows) => {
+    if (error) throw error;
+    res.send("Insert " + uid + " success");
+  });
+});
+*/
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
 });
